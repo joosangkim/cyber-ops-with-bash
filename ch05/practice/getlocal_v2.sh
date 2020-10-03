e1d1a91377f90e3e29c495140819846970c83888
jeff.kim <jeff.kim@groundx.xyz>
2020-10-03 15:24:07 +0900

    Add practice

--- ch05/practice/getlocal_v2.sh
+++ ch05/practice/getlocal_v2.sh
@@ -9,6 +9,9 @@
 # Usage:
 # bash getlocal.sh < cmds.txt
 #   cmds.txt is a file with list of commands to run
+
+# Update:
+# base getlocal.sh -i cert_path -h remote_host -u remote_user < cmds.txt
 #
 
 # SepCmds - separate the commands from the line of input
@@ -61,10 +64,30 @@ function DumpInfo ()
     printf "</systeminfo>\n"
 }
 
+function RemoteHost(){
+	while getopts 'i:h:u:' opt; do
+		case $opt in
+		  i)
+				RECERT="${OPTARG}"
+				;;
+			h)
+				REHOST="${OPTARG}"
+				;;
+			u)
+				REUSER="${OPTARG}"
+				echo "$OPTARG"
+				;;
+		esac
+	done
+}
+
 OSTYPE=$(./osdetect.sh)                     # <1>
 HOSTNM=$(hostname)                          # <2>
 TMPFILE="${HOSTNM}.info"                    # <3>
 
 # gather the info into the tmp file; errors, too
-DumpInfo > $TMPFILE  2>&1                  # <4>
+DumpInfo > $TMPFILE  2>&1            # <4>
+RemoteHost $@
+echo "scp -i ${RECERT} $TMPFILE ${REUSER}@${REHOST}:/home/ec2-user/temp/${TMPFILE}"
+scp -i ${RECERT} $TMPFILE ${REUSER}@${REHOST}:/home/ec2-user/temp/${TMPFILE}
 
